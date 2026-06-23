4fb4fe70a516518886b6d416d9683bac3644243a
Shikhar Tandon <shikhartandon19932gmail.com>
2026-06-22 21:17:00 -0700

    adding restore script

---
+++ 26020_Restore.bat
@@ -0,0 +1,10 @@
+set CLASS_NUMBER=26020
+set CLASS_NAME=26020_RTOS4
+
+echo Erasing old %CLASS_NAME% directory
+del C:\Masters\%CLASS_NAME%\* /S /F /Q
+echo Restoring class %CLASS_NAME%
+xcopy C:\Backup\%CLASS_NAME% c:\Masters\%CLASS_NAME% /F /E /C /Y /i
+echo Removing %CLASS_NUMBER%_Restore.bat file
+del "C:\Masters\%CLASS_NAME%\%CLASS_NUMBER%_Restore.bat"
+exit
